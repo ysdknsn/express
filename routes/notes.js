1c4e79a2f5be6a56bc586dff2f5bdba98f6ef584
yasuda <2001010382ur@cyber-u.ac.jp>
2023-11-28 18:05:52 +0900

    database

--- routes/notes.js
+++ routes/notes.js
@@ -1,6 +1,25 @@
 var express = require('express');
 var router = express.Router();
 
+//接続情報を設定
+const {MongoClient} = require("mongodb");
+const uri = 
+"mongodb+srv://yasusa:k20140312@sample.h71o90k.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri);
+
+router.get('/',async (req,res) => {
+    //データベース、コレクションを指定
+    const database = client.db('notes');
+    const notes = database.collection('notes');
+
+    //idが１のドキュメントを取得
+    const query = {id : 1};
+    const note = await notes.findOne(query);
+
+    res.json(note);
+})
+
+
 //レスポンスデータ（ノート全件）
 const responseObjectDataAll = {
     textObject1:{
